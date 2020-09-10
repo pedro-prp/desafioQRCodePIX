cae87ec55a028d014efc699b9bbb13faabc3686e
Pedro Rodrigues <peddroprp@gmail.com>
2020-09-10 19:40:17 -0300

    Apply pages and routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import Routes from './routes'
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>
-          Hello world
-        </div>
+        <Routes />
       </header>
     </div>
   );
---
+++ frontend/src/pages/Landing/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+function Landing() {
+    return (
+        <div id="page-landing">
+            <h1>Hello Landing</h1>
+        </div>
+    )
+}
+
+export default Landing;
