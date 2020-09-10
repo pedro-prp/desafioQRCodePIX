7434147b38a8384b2227b1edeeed8c9b449893b1
Pedro Rodrigues <peddroprp@gmail.com>
2020-09-10 20:42:18 -0300

    Get input values to variables

--- frontend/src/pages/Landing/index.tsx
+++ frontend/src/pages/Landing/index.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
-// import { Link } from 'react-router-dom';
+import React, { useCallback } from 'react';
+import api from '../../services/api';
 
-function Landing() {
+const Landing: React.FC = () => {
+
+    const createQRCode = (): void => {
+        let payer = (document.getElementById('payer-id') as HTMLInputElement).value
+        let receiver = (document.getElementById('receiver-id') as HTMLInputElement).value
+        let value = (document.getElementById('value') as HTMLInputElement).value
+
+        console.log(payer + receiver + value);
+    };
+
     return (
         <div id="page-landing">
-            <h1>Hello Landing</h1>
+            <div className="Input">
+                <div>
+                    <p>Payer ID</p>
+                    <input type="text" id='payer-id' name='Payer ID' />
+                </div>
+                <div>
+                    <p>Receiver ID</p>
+                    <input type="text" id='receiver-id' name='Receiver ID' />
+                </div>
+                <div>
+                    <p>Value</p>
+                    <input type="text" id='value' name='value' />
+                </div>
+            </div>
+            <button onClick={createQRCode}>Generate QR code</button>
         </div>
     )
 }
