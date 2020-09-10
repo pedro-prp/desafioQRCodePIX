eba7b91310262cc710ac0ac12daa78541bde76c2
Pedro Rodrigues <peddroprp@gmail.com>
2020-09-10 14:45:10 -0300

    Add controllers structure

---
+++ backend/src/controllers/transaction.ts
@@ -0,0 +1,7 @@
+class transactionController {
+    public createQRcode = () => {
+        return ({'qrcode': 'hello controllers'})
+    }
+}
+ 
+export default transactionController;
--- backend/src/routes/transaction.routes.ts
+++ backend/src/routes/transaction.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 
+import transactionInterface from '../interfaces/transactionInterface';
+import transactionController from '../controllers/transaction';
+
 const transactionRouter = Router();
 
 transactionRouter.post('/', async (request, response) => {
   try {
-    return response.status(200).json({"hello": "world"})
+    var obj = new transactionController()
+
+    return response.status(200).json(obj.createQRcode())
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
